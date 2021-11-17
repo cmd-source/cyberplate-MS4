c527ab8a42f3d98b87637e567c52e411da66a230
cmd-source <conorcrosbie09@gmail.com>
2021-11-17 10:09:12 +0000

    added in signals to the checout

---
+++ checkout/signals.py
@@ -0,0 +1,21 @@
+
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderItem
+
+#These signals were taken directly from the Code Institutes walkalong project Boutique Ado
+
+@receiver(post_save, sender=OrderItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on lineitem update/create
+    """
+    instance.order.order_total()
+
+@receiver(post_delete, sender=OrderItem)
+def update_on_save(sender, instance, **kwargs):
+    """
+    Update order total on lineitem delete
+    """
+    instance.order.order_total()
