7357a9b1b9687d1e6cd11bc9ee7182ad4d44d941
cmd-source <conorcrosbie09@gmail.com>
2021-12-03 13:29:04 +0000

    added the toast files and starter templates

--- profiles/forms.py
+++ profiles/forms.py
@@ -24,3 +24,14 @@ class UsersProfileForm(forms.ModelForm):
                 'def_town': 'town',
                 'def_country': 'country',
             }
+
+            self.fields['def_first_name'].widget.attrs['autofocus'] = True
+            for field in self.fields:
+                if field != 'default_country':
+                    if self.fields[field].required:
+                        placeholder = f'{placeholders[field]} *'
+                    else:
+                        placeholder = placeholders[field]
+                    self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+                self.fields[field].label = False
