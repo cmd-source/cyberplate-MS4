bff0c886d903556b2fc3dac6da33a5c0f82cc2bf
cmd-source <conorcrosbie09@gmail.com>
2021-12-05 19:28:53 +0000

    both the update and remove functions now work on the shopping bag

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('shopping_bag', views.shopping_bag, name='shopping_bag'),
-    path('add/<item_id>', views.add_product_to_bag, name='add_product_to_bag')
+    path('add/<item_id>', views.add_product_to_bag, name='add_product_to_bag'),
+    path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
+    path('remove/<item_id>/', views.remove, name='remove')
 ]
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.conf import settings
 from products.models import Product
 from django.contrib import messages
@@ -30,3 +30,35 @@ def add_product_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+        messages.info(request, f'You have updated your bag')
+    else:
+        bag.pop(item_id)
+        messages.info(request, f'You have updated your bag')
+
+    request.session['bag'] = bag
+    return redirect(reverse('shopping_bag'))
+
+
+
+def remove(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    bag = request.session.get('bag', {})
+
+    bag.pop(item_id)
+    messages.info(request, f'You have removed from your bag')
+
+
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
