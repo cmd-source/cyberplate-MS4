51e0a349c431c73fd8fa78e0797e4d71edf528e8
cmd-source <conorcrosbie09@gmail.com>
2021-12-08 18:20:25 +0000

    adding in email functionality for registration

--- cyberplate/settings.py
+++ cyberplate/settings.py
@@ -211,3 +211,15 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_PRIVATE_KEY = os.getenv('STRIPE_PRIVATE_KEY', '')
 STRIPE_WEBHOOK_KEY = os.getenv('STRIPE_WEBHOOK_KEY', '')
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
