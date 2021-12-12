417067bf11dfb059041752ac69cac59edad8f836
cmd-source <conorcrosbie09@gmail.com>
2021-12-12 19:18:52 +0000

    trying to undo an error made

--- checkout/models.py
+++ checkout/models.py
@@ -32,15 +32,6 @@ class Order(models.Model):
 
         return uuid.uuid4().hex.upper()
 
-    def update_total(self):
-        """
-        Update grand total each time a line item is added,
-        accounting for delivery costs.
-        """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
-        self.grand_total = self.order_total + self.delivery_cost
-        self.save()
-
     def save(self, *args, **kwargs):
 
         if not self.users_order_number:
