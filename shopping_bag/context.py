ba67e6a3e8d85a4543015bf0916f5843895fe95f
cmd-source <conorcrosbie09@gmail.com>
2021-12-12 17:24:58 +0000

    add some small improvements to the navbar, buttons on the product view and on the bag view

--- shopping_bag/context.py
+++ shopping_bag/context.py
@@ -39,7 +39,7 @@ def bag_contents(request):
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = 10
+        delivery = 0
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
